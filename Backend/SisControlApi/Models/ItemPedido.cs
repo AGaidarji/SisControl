20e42021d2224a6d56309317916fb19480d2e36f
Guilherme Bomfim Dev <guigui_bsg@hotmail.com>
2024-10-10 22:30:10 -0300

    Backend

---
+++ Backend/SisControlApi/Models/ItemPedido.cs
@@ -0,0 +1,10 @@
+﻿namespace SisControlApi.Models
+{
+    public class ItemPedido
+    {
+        public string NomeItem { get; set; }
+        public int Quantidade { get; set; }
+        public string Evento { get; set; }
+        public DateTime DataEvento { get; set; }
+    }
+}
