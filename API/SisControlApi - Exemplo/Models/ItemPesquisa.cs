6544d660315d60e05a274449cf4ae81cbc616e78
Guilherme Bomfim Dev <guigui_bsg@hotmail.com>
2024-10-31 11:56:31 -0300

    Adição de um Exemplo da API ao github

---
+++ API/SisControlApi - Exemplo/Models/ItemPesquisa.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SisControlApi.Models
+{
+    public class ItemPesquisa
+    {
+        [Key]
+        public int IdItem { get; set; }
+        public string NomeItem { get; set; }
+        public int? Codigo { get; set; }
+    }
+}
