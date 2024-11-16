a7032fe20dc996525fc6cb888a69875af9a461db
Guilherme Bomfim Dev <guigui_bsg@hotmail.com>
2024-11-16 18:02:29 -0300

    v2.0.0 - Mudanca no visual
    Branch - Developer
    Changes - Mudancas no visual do Main Page

--- API/SisControlApi - Exemplo/obj/Debug/net8.0/SisControlApi.AssemblyInfo.cs
+++ API/SisControlApi - Exemplo/obj/Debug/net8.0/SisControlApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SisControlApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000a5dd2662c4f5c3eae341329b745a7db4445f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f713bd0c58da3c047f5f7017393c3345e51e8726")]
 [assembly: System.Reflection.AssemblyProductAttribute("SisControlApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SisControlApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
